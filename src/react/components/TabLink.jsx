c33d623651b7785684a85ffd521dbe94e2697f40
Michel Bobillier <athos99@bluewin.ch>
2024-01-18 14:58:34 +0000

    update

--- src/react/components/TabLink.jsx
+++ src/react/components/TabLink.jsx
@@ -4,16 +4,18 @@ import { NavLink as ReactRouterLink, useMatch } from "react-router-dom";
 import React from "react";
 
 const TabLink = (allProps) => {
-  allProps.value = allProps.href;
   const match = useMatch(allProps.href);
   console.log("TabLink " + allProps.href, allProps);
+
+  
   //console.log("tablink " + allProps.href, match ? true : false);
   return (
     <MuiTab
-      {...allProps}
+      value = { allProps.href}
       component={ReactRouterLink}
       to={allProps.href ?? "#"}
       selected={match ? true : false}
+      {...allProps}
     />
   );
 };
